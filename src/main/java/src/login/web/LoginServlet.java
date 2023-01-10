78a3fc9caa5aa77260d4f4a27ce7bad453a74087
RobinsonConcha <121482451+RobinsonConcha@users.noreply.github.com>
2023-01-10 06:19:35 -0400

    correccion servlet

--- src/main/java/src/login/web/LoginServlet.java
+++ src/main/java/src/login/web/LoginServlet.java
@@ -1,11 +1,7 @@
 package src.login.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,56 +12,56 @@ import src.login.database.LoginDao;
 
 /**
  * Servlet implementation class LoginServlet
- 
-public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+ */
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	public class LoginServlet extends HttpServlet {
+		private static final long serialVersionUID = 1L;
+	       
+	    /**
+	     * @see HttpServlet#HttpServlet()
+	     */
+	    public LoginServlet() {
+	        super();
+	        // TODO Auto-generated constructor stub
+	    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		LoginDao loginDao = new LoginDao();
-		
-
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-		LoginBean usuario = new LoginBean();
-		usuario.setUsername(username);
-		usuario.setPassword(password);
-
-		if (loginDao.validate(usuario)==false) {
-			
-			//request.setAttribute("username", username);
-		    //RequestDispatcher rd= request.getRequestDispatcher("principal.jsp");
-			  //PrintWriter out=response.getWriter();
-			  // out.println("Bienvenido:"+username);
-		    //response.sendRedirect("principal.jsp");
-		       //HttpSession session = request.getSession();
-			  //session.setAttribute("user", username);
-			 response.sendRedirect("index.jsp");
-		   //rd.forward(request, response);
-			
-		} else {
-			
-			HttpSession session = request.getSession();
-			session.setAttribute("user", username);
-			response.sendRedirect("principal.jsp");
+		/**
+		 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+		 */
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			// TODO Auto-generated method stub
+			response.getWriter().append("Served at: ").append(request.getContextPath());
 		}
-	}
+
+		/**
+		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+		 */
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			LoginDao loginDao = new LoginDao();
+			
+			String username = request.getParameter("username");
+			String password = request.getParameter("password");
+			LoginBean loginBean = new LoginBean();
+			loginBean.setUsername(username);
+			loginBean.setPassword(password);
+			
+			if (loginDao.validate(loginBean))
+			{
+				HttpSession session = request.getSession();
+			    session.setAttribute("user",username);
+			   //response.sendRedirect("principal.jsp");
+			
+				response.sendRedirect("principal.jsp");
+				
+				
+			}
+			else 
+			{
+				//HttpSession session = request.getSession();
+				response.sendRedirect("index.jsp");
+				
+			}
+			
+		}
+
 }
